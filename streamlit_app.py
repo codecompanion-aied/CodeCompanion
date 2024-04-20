c01c23cff3cc53873c366c9551a868e445d3c6b9
Francesco Kruk <46400934+francesco-kruk@users.noreply.github.com>
2024-04-20 15:14:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+$ pip install streamlit
 
+import streamlit as st
+
+# Insert a chat message container.
+with st.chat_message("user"):
+  st.write("Hello 👋")
+  st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("Say something")
